3087c23c218e3e795dadd90628880ed362a2b3ad
Caelix <caelixorbit@gmail.com>
2026-04-03 08:49:28 +0800

    feat: hook TDIRecipeAndResearchLoader

--- src/main/java/ThaumicDualityInterface/ThaumicDualityInterface.java
+++ src/main/java/ThaumicDualityInterface/ThaumicDualityInterface.java
@@ -1,25 +1,22 @@
 package ThaumicDualityInterface;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import ThaumicDualityInterface.inventory.InventoryHandler;
 import ThaumicDualityInterface.loader.ItemAndBlockHolder;
+import ThaumicDualityInterface.loader.TDIRecipeAndResearchLoader;
 import ThaumicDualityInterface.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(
     modid = ThaumicDualityInterface.MODID,
     version = Tags.VERSION,
     name = ThaumicDualityInterface.MODNAME,
     acceptedMinecraftVersions = "[1.7.10]",
-    dependencies = "required-after:appliedenergistics2;required-after:thaumicenergistics")
+    dependencies = "required-after:Thaumcraft;required-after:appliedenergistics2;required-after:thaumicenergistics")
 public class ThaumicDualityInterface {
 
     public static final String MODID = "thaumicdualityinterface";
@@ -50,6 +47,14 @@ public class ThaumicDualityInterface {
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void loadComplete(FMLLoadCompleteEvent event) {
+        try {
+            TDIRecipeAndResearchLoader.postInit();
+        } catch (Exception e) {
+        }
     }
 
     @Mod.EventHandler
